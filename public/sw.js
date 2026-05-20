191fd43b5a9027ad5b853f1c997618c22e750837
Иван <nerealniylubovnik123@gmail.com>
2026-05-20 15:07:41 +0300

    12

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'detki-v-polyane-v2'
+const cacheName = 'detki-v-polyane-v3'
 const files = ['/', '/owner', '/manifest.webmanifest', '/icon.svg', '/icon-192.png', '/icon-512.png']
 
 self.addEventListener('install', event => {
@@ -13,6 +13,11 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return
+  const url = new URL(event.request.url)
+  if (url.pathname.startsWith('/api/')) {
+    event.respondWith(fetch(event.request))
+    return
+  }
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
     const copy = response.clone()
     caches.open(cacheName).then(cache => cache.put(event.request, copy))
