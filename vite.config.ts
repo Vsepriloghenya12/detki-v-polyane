9a24db5d9a5032af5fb2007dcab18186f3b7582c
Иван <nerealniylubovnik123@gmail.com>
2026-05-23 16:13:08 +0300

    12

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  }
 })
